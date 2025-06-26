07421a12772288b546b02d5e3af1562c537143db
Kauane1706 <kauanekakadossantos854gmail.com>
2025-06-26 08:55:53 -0300

    substitui imagens de Pokémon e atualiza referências no HTML e JavaScript

--- scripts/pokemons.js
+++ scripts/pokemons.js
@@ -1,6 +1,6 @@
 function trocarcharmander(){
   document.getElementById("pokemon-name").innerHTML = "Charmander";
-  document.getElementById("pokemon-img").src = "imagens/Charmander.png";
+  document.getElementById("pokemon-img").src = "imagens/Charmander (2).png";
   document.getElementById("attacks-list").innerHTML = "<li>Chama</li><li>Arranhão</li><li>Golpe de Cauda</li>";
   document.getElementById("pokemon-type").innerHTML = "Tipo: Fogo";
   document.getElementById("pokemon-card").style.backgroundColor = "red"; // Cor de fundo para Charmander
@@ -8,7 +8,7 @@ function trocarcharmander(){
 
 function trocarsquirtle(){
   document.getElementById("pokemon-name").innerHTML = "Squirtle";
-  document.getElementById("pokemon-img").src = "imagens/Squirtle.jpg";
+  document.getElementById("pokemon-img").src = "imagens/Squirtle (3).png";
   document.getElementById("attacks-list").innerHTML = "<li>Jato d'Água</li><li>Investida</li><li>Giro Rápido</li>";
   document.getElementById("pokemon-type").innerHTML = "Tipo: Água";
   document.getElementById("pokemon-card").style.backgroundColor = "blue"; // Cor de fundo para Squirtle
